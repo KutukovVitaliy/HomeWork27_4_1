249810d9c353913d64708d2129330712ad5beaf0
kvit <kutukov@list.ru>
2021-11-09 21:17:42 +0300

    next commit

--- main.cpp
+++ main.cpp
@@ -20,20 +20,46 @@ class BigBranch{
     std::string elfName;
 public:
     BigBranch(class Tree* inTree, std::string name): tree(inTree), elfName(name){
+        assert(inTree != nullptr);
         countMiddleBranch = (std::rand() % 2) + 2;
         pMiddleBranch = new MiddleBranch*[countMiddleBranch];
         for(int i = 0; i < countMiddleBranch; ++i){
             pMiddleBranch[i] = new MiddleBranch(this, "None");
         }
     }
+    std::string GetElfName(){
+        return this->elfName;
+    }
+    void SetElfName(std::string name){
+        this->elfName = name;
+    }
 };
 
 class Tree{
     int countBigBranch;
     BigBranch** pBigBranch = nullptr;
+public:
+    Tree(){
+        countBigBranch = (std::rand() % 3) + 3;
+        pBigBranch = new BigBranch*[countBigBranch];
+        for(int i = 0; i < countBigBranch; ++i){
+            pBigBranch[i] = new BigBranch(this, "None");
+        }
+    }
+    BigBranch* GetBigBranchAt(int index){
+        return this->pBigBranch[index];
+    }
+    int GetBigBranchCount(){
+        return this->countBigBranch;
+    }
 };
 
 int main() {
+    Tree myTree;
+    for(int i = 0; i < myTree.GetBigBranchCount(); ++i){
+        myTree.GetBigBranchAt(i)->SetElfName(std::string("Elf" + std::to_string(i+1)));
+        std::cout << "Big branch #" << i << " Elf name is " << myTree.GetBigBranchAt(i)->GetElfName() << std::endl;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
